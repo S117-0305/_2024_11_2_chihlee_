8abc653c31be145cc0e89a34ea5a8e6ff9357041
S117-0305 <emile0305@gmail.com>
2024-12-07 07:06:43 +0000

    modify

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -1,9 +1,17 @@
 from pprint import pprint
+import openpyxl
+from openpyxl import Workbook, worksheet
 import tools
 
 def main():
-    data:list[dict] = tools.get_aqi(excel_name="aqi.xlsx")
-    pprint(data)
-
+    sitenames:list[str] = tools.get_sitenames(excel_name="aqi.xlsx")
+    wb: Workbook = openpyxl.Workbook()
+    sheet:worksheet = wb.active
+    sheet.title = "站點名稱"
+    for idy , name in enumerate(sitenames):
+        print(name)
+        sheet.cell(column=1,row=idy+1,value=name)
+    wb.save("老闆要的資訊.xlsx")
+        
 if __name__ == "__main__":
     main()
