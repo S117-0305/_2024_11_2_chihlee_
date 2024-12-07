29deae5b1ad030e11cba6c3e3f20e1d012b7d14a
S117-0305 <emile0305@gmail.com>
2024-12-07 06:09:41 +0000

    modify

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -1,20 +1,8 @@
-from openpyxl import load_workbook, workbook, worksheet
 from pprint import pprint
-
-def get_aqi(excel_name:str)-> list[dict]:
-    wb:workbook = load_workbook("aqi.xlsx")
-    from pprint import pprint
-    sheet:worksheet = wb.worksheets[0]
-    sheets:list = []
-    column_names:list[str]=[cell.value for cell in list(sheet)[0]]
-    for row in list(sheet)[1:]:
-        site:dict = {column_names[idx]:cell.value for idx,cell in enumerate(row)}    
-        sheets.append(site)
-
-    return sheets
+import tools
 
 def main():
-    data:list[dict] = get_aqi(excel_name="aqi.xlsx")
+    data:list[dict] = tools.get_aqi(excel_name="aqi.xlsx")
     pprint(data)
 
 if __name__ == "__main__":
