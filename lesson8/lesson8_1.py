8ccbd620602585e72fe4fe8ba199c3ffef1f680b
S117-0305 <emile0305@gmail.com>
2024-12-21 02:23:49 +0000

    modify

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -5,8 +5,10 @@ if "counter" not in st.session_state:
     #st.session_state.[counter] = 0
     st.session_state.counter = 1
 
-def increment_counter():
-    st.session_state.counter += 1
+def increment_counter(*args):
+    st.session_state.counter += args[0]
+    st.session_state.counter -= args[1]
+
 
 st.header(f"這個網頁已經執行了{st.session_state.counter}次")  
-st.button("再執行一次", key="reset", help="按下按鈕後，計數器會加1", on_click=increment_counter)
+st.button("再執行一次", key="reset", help="按下按鈕後，計數器會加1", on_click=increment_counter, args=(5,3))
