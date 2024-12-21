9b1c478a3622ffbe65dbd327a13113a9c824ace3
S117-0305 <emile0305@gmail.com>
2024-12-21 02:29:35 +0000

    modify

---
+++ lesson8/lesson8_2.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+#手動建立counter變數，並設定初始值為0
+if "counter" not in st.session_state:
+    #st.session_state.[counter] = 0
+    st.session_state.counter = 1
+
+def increment_counter(**kwargs):
+    st.session_state.counter += kwargs["first"]
+    st.session_state.counter -= kwargs["second"]
+
+
+st.header(f"這個網頁已經執行了{st.session_state.counter}次")  
+st.button("再執行一次", key="reset", help="按下按鈕後，計數器會加1", on_click=increment_counter, kwargs={"first":5,"second":3})
