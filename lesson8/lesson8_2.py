91a7fa5c3d0e6d412c8f5c15991a9d2671efcc8d
S117-0305 <emile0305@gmail.com>
2024-12-21 04:01:55 +0000

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -11,4 +11,8 @@ def increment_counter(**kwargs):
 
 
 st.header(f"這個網頁已經執行了{st.session_state.counter}次")  
-st.button("再執行一次", key="reset", help="按下按鈕後，計數器會加1", on_click=increment_counter, kwargs={"first":5,"second":3})
+st.button("再執行一次",
+           key="reset", 
+           help="按下按鈕後，計數器會加1", 
+           on_click=increment_counter, 
+           kwargs={"first":5,"second":3})
